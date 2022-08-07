b1aeac60a10347da0baa3d197ad0607f6d342272
Ankur Jhavery <ankur@dhamika.com>
2022-08-08 00:04:13 +0530

    initiated google maps react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import { formatRelative } from 'date-fns';
+import mapStyles from './map-styles';
 
-function App() {
+/**************** MAP VARIABLES *************/
+
+// declare outside to prevent too many re renders
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 28.7041,
+  lng: 77.1025,
+};
+// Find map styles on: https://snazzymaps.com/style/8097/wy
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true, // to remove default map UI controls
+  zoomControl: true, // to add zoom control
+};
+
+const App = () => {
+  // load script hook - loads the google maps script and libraries using API key
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return 'Error loading maps';
+  if (!isLoaded) return 'Loading maps';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+      >
+        <Marker position={{ lat: 28.7041, lng: 77.1025 }} />
+      </GoogleMap>
     </div>
   );
-}
+};
 
 export default App;
